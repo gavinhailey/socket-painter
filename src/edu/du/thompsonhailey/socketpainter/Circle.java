f835de24cb68e2f8840725fb765e9eaa20cbb3b5
Giovanni Thompson <giovanni.thompson@du.edu>
2020-11-28 14:47:30 -0700

    Finished Line and Circle

--- src/edu/du/thompsonhailey/socketpainter/Circle.java
+++ src/edu/du/thompsonhailey/socketpainter/Circle.java
@@ -1,4 +1,18 @@
-package edu.du.thompsonhailey.socketpainter;
+import java.awt.*;
 
-public class Circle {
+public class Circle extends PaintingPrimitive{
+    private Point center;
+    private Point radiusPoint;
+
+    public Circle(Color c, Point center, Point radiusPoint){
+        super(c);
+        this.center = center;
+        this.radiusPoint = radiusPoint;
+    }
+
+    @Override
+    public void drawGeometry(Graphics g) {
+        int radius = (int) Math.abs(center.distance(radiusPoint));
+        g.drawOval(center.x - radius, center.y - radius, radius*2, radius*2);
+    }
 }
--- src/edu/du/thompsonhailey/socketpainter/Line.java
+++ src/edu/du/thompsonhailey/socketpainter/Line.java
@@ -1,4 +1,17 @@
-package edu.du.thompsonhailey.socketpainter;
+import java.awt.*;
 
-public class Line {
+public class Line extends PaintingPrimitive{
+    private Point start;
+    private Point end;
+
+    public Line(Color c, Point start, Point end){
+        super(c);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void drawGeometry(Graphics g) {
+        g.drawLine(start.x, start.y, end.x, end.y);
+    }
 }

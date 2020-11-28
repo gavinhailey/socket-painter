a865d46739e0787b4e369ecf109ff9d7364cebe1
Giovanni Thompson <giovanni.thompson@du.edu>
2020-11-28 15:30:36 -0700

    Finished Painting Panel

--- src/edu/du/thompsonhailey/socketpainter/PaintingPanel.java
+++ src/edu/du/thompsonhailey/socketpainter/PaintingPanel.java
@@ -1,5 +1,25 @@
 package edu.du.thompsonhailey.socketpainter;
 
-public class PaintingPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class PaintingPanel extends JPanel {
+    private ArrayList<PaintingPrimitive> list = new ArrayList<>();
+
+    public PaintingPanel(){
+        this.setBackground(Color.WHITE);
+    }
+
+    public void addPrimitive(PaintingPrimitive obj) {
+        this.list.add(obj);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for(PaintingPrimitive obj : list) {
+            obj.draw(g);
+        }
+    }
 
 }

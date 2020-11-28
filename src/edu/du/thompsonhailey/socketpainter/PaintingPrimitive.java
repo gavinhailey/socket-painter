4cd44a3683744ce864d867b420d00b2adba8aaa6
Giovanni Thompson <giovanni.thompson@du.edu>
2020-11-28 14:35:28 -0700

    Finished PaintingPrimitive

--- src/edu/du/thompsonhailey/socketpainter/PaintingPrimitive.java
+++ src/edu/du/thompsonhailey/socketpainter/PaintingPrimitive.java
@@ -1,4 +1,15 @@
-package edu.du.thompsonhailey.socketpainter;
+import java.awt.*;
 
-public class PaintingPrimitive {
+public abstract class PaintingPrimitive {
+    Color c;
+    public PaintingPrimitive(Color c){
+        this.c = c;
+    }
+
+    public final void draw(Graphics g) {
+        g.setColor(this.c);
+        drawGeometry(g);
+    }
+
+    protected abstract void drawGeometry(Graphics g);
 }

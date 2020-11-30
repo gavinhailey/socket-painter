9351ce16c0b646555819700577513ffeaaaf1cb1
Gavin Hailey <gavin.hailey@du.edu>
2020-11-29 20:01:01 -0500

    Finished chat

--- src/edu/du/thompsonhailey/socketpainter/Painter.java
+++ src/edu/du/thompsonhailey/socketpainter/Painter.java
@@ -1,18 +1,33 @@
 package edu.du.thompsonhailey.socketpainter;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class Painter extends JFrame implements ActionListener, MouseListener, MouseMotionListener {
     private Color colorSelection;
     private String shapeSelection;
     private Point startPoint;
     private PaintingPanel paintingPanel;
+    private String name;
+    private ArrayList<String> chatText;
+    private JTextArea chatArea;
+    private JTextField messageField;
+    private Socket socket;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
 
     public Painter() {
         this.shapeSelection = "line";
         this.colorSelection = Color.RED;
+        chatText = new ArrayList<>();
 
         setSize(500, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -51,36 +66,70 @@ public class Painter extends JFrame implements ActionListener, MouseListener, Mo
         bluePaint.addActionListener(this);
         paintPanel.add(bluePaint);
 
+        //create shapes panel
         JPanel shapePanel = new JPanel();
         shapePanel.setLayout(new GridLayout(1, 2));
 
+        // add line button
         JButton lineButton = new JButton("Line");
         lineButton.setActionCommand("line");
         lineButton.addActionListener(this);
         shapePanel.add(lineButton);
 
+        //add circle button
         JButton circleButton = new JButton("Circle");
         circleButton.setActionCommand("circle");
         circleButton.addActionListener(this);
         shapePanel.add(circleButton);
 
+        //creates chat panel
+        JPanel chatPanel = new JPanel();
+        chatPanel.setLayout(new BorderLayout());
+
+        //creates messagebox
+        JLabel messageLabel = new JLabel("Message");
+        messageLabel.setBorder(new EmptyBorder(3,3,3,3));
+        messageField = new JTextField();
+        JButton sendButton = new JButton("Send");
+        sendButton.setActionCommand("send");
+        sendButton.addActionListener(this);
+        sendButton.setBorder(new EmptyBorder(3,3,3,3));
+        chatArea = new JTextArea(3,100);
+        chatArea.setRows(3);
+        chatArea.setMaximumSize(new Dimension(500,100));
+        JScrollPane chatScroll = new JScrollPane(chatArea);
+        chatScroll.setVerticalScrollBarPolicy(20);
+        chatScroll.setHorizontalScrollBarPolicy(31);
+        chatScroll.setPreferredSize(new Dimension(500,100));
+        chatScroll.setBorder(new TitledBorder("Chat"));
+        chatPanel.add(messageLabel, BorderLayout.WEST);
+        chatPanel.add(messageField, BorderLayout.CENTER);
+        chatPanel.add(sendButton, BorderLayout.EAST);
+        chatPanel.add(chatScroll, BorderLayout.NORTH);
+
         this.paintingPanel = new PaintingPanel();
         paintingPanel.addMouseListener(this);
         paintingPanel.addMouseMotionListener(this);
-        holder.add(paintingPanel, BorderLayout.CENTER);
 
         // add the panels to the overall panel, holder
         holder.add(paintPanel, BorderLayout.WEST);
         holder.add(shapePanel, BorderLayout.NORTH);
-
-
-        //TODO: And later you will add the chat panel to the SOUTH
+        holder.add(chatPanel, BorderLayout.SOUTH);
+        holder.add(paintingPanel, BorderLayout.CENTER);
 
         // Lastly, connect the holder to the JFrame
         setContentPane(holder);
 
         // And make it visible to layout all the components on the screen
         setVisible(true);
+
+        try {
+            socket = new Socket("localhost", 6969);
+            ois = new ObjectInputStream(socket.getInputStream());
+            oos = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -88,15 +137,26 @@ public class Painter extends JFrame implements ActionListener, MouseListener, Mo
         System.out.println("Action Event: " + actionEvent.getActionCommand());
         String s = actionEvent.getActionCommand();
         if (s.equals("red"))
-            this.colorSelection = Color.RED;
+            colorSelection = Color.RED;
         else if (s.equals("green"))
-            this.colorSelection = Color.GREEN;
+            colorSelection = Color.GREEN;
         else if (s.equals("blue"))
-            this.colorSelection = Color.BLUE;
+            colorSelection = Color.BLUE;
         else if (s.equals("line"))
-            this.shapeSelection = "line";
+            shapeSelection = "line";
         else if (s.equals("circle"))
-            this.shapeSelection = "circle";
+            shapeSelection = "circle";
+        else if (s.equals("send")) {
+            String msg = name + ": " + messageField.getText() + "\n";
+            chatText.add(msg);
+            chatArea.append(msg);
+            chatArea.updateUI();
+            try {
+                oos.writeObject(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -121,6 +181,8 @@ public class Painter extends JFrame implements ActionListener, MouseListener, Mo
             System.out.println("adding circle");
             Circle c = new Circle(this.colorSelection, this.startPoint, mouseEvent.getPoint());
             paintingPanel.addPrimitive(c);
+        } else {
+            System.out.println("Invalid shape!");
         }
         paintingPanel.updateUI();
     }
@@ -146,6 +208,9 @@ public class Painter extends JFrame implements ActionListener, MouseListener, Mo
     }
 
     public static void main(String[] args) {
-        new Painter();
+        Painter painter = new Painter();
+        painter.name = JOptionPane.showInputDialog("Enter your name:");
+        if (painter.name == null)
+            System.exit(1);
     }
 }
